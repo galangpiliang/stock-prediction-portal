ef24a5a5eb0163872c13e517287a8b4c2771bdf0
Galang <galangpiliang@gmail.com>
2025-09-27 12:14:06 +0700

    User Serializer

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    
