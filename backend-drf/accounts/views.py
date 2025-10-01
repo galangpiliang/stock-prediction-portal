7f55cda1aab1ca39f97f33fa6344a6d537f1e9d1
Galang <galang.pil@gmail.com>
2025-10-01 12:50:40 +0700

    Register View

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import UserSerializers
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [AllowAny]
