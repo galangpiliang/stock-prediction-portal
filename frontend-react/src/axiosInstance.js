18d92d67a683689fa7c393a4b3e05cd33df05086
Galang <galangpiliang@gmail.com>
2025-10-13 09:22:40 +0700

    Using Request Interceptor

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -3,7 +3,26 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BACKEND_BASE_API
 
 const axiosInstance = axios.create({
-    baseURL: baseURL,
+	baseURL: baseURL,
+	headers: {
+		'Content-Type': 'application/json',
+	}
 })
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+	function (config) {
+		const accessToken = localStorage.getItem('accessToken')
+
+		if (accessToken) {
+			config.headers['Authorization'] = `Bearer ${accessToken}`
+		}
+		console.log(config)
+
+		return config;
+	}, function (error) {
+		return Promise.reject(error);
+	}
+);
 
 export default axiosInstance
