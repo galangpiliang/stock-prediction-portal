9c2c557b343945d17ef767aa41e35f0dfe4a69e1
Galang <galang.pil@gmail.com>
2025-11-19 11:24:13 +0700

    Generating Plot With AGG Backends

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import os
+from django.conf import settings
 
 class StockPredictionAPIView(APIView):
     def post(self, request):
@@ -20,10 +22,28 @@ class StockPredictionAPIView(APIView):
             start = datetime(now.year-10, now.month, now.day)
             df = yf.download(ticker, start, end=now)
             df.columns = df.columns.get_level_values(0)
-            print(df.head())
 
             if df.empty:
                 return Response({"error": "Invalid ticker or no data found."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            df = df.reset_index()
 
-            return Response({"message": f"Received ticker: {ticker}"}, status=status.HTTP_200_OK)
+            # Generate Basic Plot
+            plt.switch_backend('Agg')  # Use a non-interactive backend
+            plt.figure(figsize=(14,7))
+            plt.plot(df['Date'], df['Close'], label='Close Price')
+            plt.title(F'{ticker.upper()} Stock Price Over Time')
+            plt.xlabel('Date')
+            plt.ylabel('Price (USD)')
+            plt.legend()
+            plt.show()
+
+            # Save plot to a file
+            plot_filename = f'{ticker.upper()}_plot.png'
+            image_path = os.path.join(settings.MEDIA_ROOT, plot_filename)
+            plt.savefig(image_path)
+            plt.close()
+            img_url = f"{settings.MEDIA_URL}{plot_filename}"
+
+            return Response({"message": f"Received ticker: {ticker}", "plot_url": img_url}, status=status.HTTP_200_OK)
         
